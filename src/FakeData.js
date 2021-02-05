6444358c32c2654f44154e94322d096bc4785d4b
leroy <leeroynicolas62@gmail.com>
2021-02-05 22:38:25 +0100

    eslint config

--- src/FakeData.js
+++ src/FakeData.js
@@ -1,37 +1,35 @@
 const posts = [
-    {
-        id : 1,
-        name : 'Nico',
-        date : Date.now(),
-        content : 'blabla',
-        likes : 2,
-        comments : 4,
-    },
-    {
-        id : 2,
-        name : 'rclens',
-        date : Date.now(),
-        content : 'marseille',
-        likes : 20,
-        comments : 2,
-    },
-    {
-        id : 3,
-        name : 'anna',
-        date : Date.now(),
-        content : 'nice',
-        likes : 2,
-        comments : 6,
-    },
-    {
-        id : 4,
-        name : 'matis',
-        date : Date.now(),
-        content : 'paris est magique',
-        likes : 12,
-        comments : 8,
-    }
-
-
-]
+  {
+    id: 1,
+    name: 'Nico',
+    date: Date.now(),
+    content: 'blabla',
+    likes: 2,
+    comments: 4,
+  },
+  {
+    id: 2,
+    name: 'rclens',
+    date: Date.now(),
+    content: 'marseille',
+    likes: 20,
+    comments: 2,
+  },
+  {
+    id: 3,
+    name: 'anna',
+    date: Date.now(),
+    content: 'nice',
+    likes: 2,
+    comments: 6,
+  },
+  {
+    id: 4,
+    name: 'matis',
+    date: Date.now(),
+    content: 'paris est magique',
+    likes: 12,
+    comments: 8,
+  },
+];
 export default posts;
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,17 +1,31 @@
 import React, { Component } from 'react';
 
 class Post extends Component {
-    render() {
-        return (
-            <div>
-               <div className="name">{this.props.name}</div>
-               <div className="date">{this.props.date}</div>
-               <div className="content">{this.props.content}</div>
-               <div className="likes">{this.props.likes}</div>
-               <div className="comments">{this.props.comments}</div>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <div className="name">{this.props.name}</div>
+        <div className="date">{this.props.date}</div>
+        <div className="content">{this.props.content}</div>
+        <div className="likes">{this.props.likes}</div>
+        <div className="comments">{this.props.comments}</div>
+      </div>
+    );
+  }
 }
 
 export default Post;
+
+// export class Post extends Component {
+//   render() {
+//     return (
+//       <div>
+//         <div className="name">{this.props.name}</div>
+//         <div className="date">{this.props.date}</div>
+//         <div className="content">{this.props.content}</div>
+//         <div className="likes">{this.props.likes}</div>
+//         <div className="comments">{this.props.comments}</div>
+//       </div>
+//     );
+//   }
+// }
