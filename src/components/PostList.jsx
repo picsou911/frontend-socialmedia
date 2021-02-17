ddf36b582809c74563dd5798f7fd772060647abe
leroy <leeroynicolas62@gmail.com>
2021-02-17 21:49:57 +0100

    map ok with j-son

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,33 +1,35 @@
 import React, { Component } from 'react';
-import Datas from '../FakeData';
+//import Datas from '../FakeData';
 import Post from './Post';
 
 class PostList extends Component {
-   
     constructor(props){
        super(props)
        this.state = {
-           posts : []
+          posts : []
        }
    }
    componentDidMount (){
-       this.setState({posts : Datas})
+      fetch ('http://localhost:8000/posts')
+        .then(response => response.json())
+        .then(data => {this.setState({posts : data })})
    }
-    render() {
-        
-       const postComponents =  Datas.map(post => (
-            <Post
-            key = {'post-'+post.id}
-            name = {post.name}
-            date = {post.date}
-            content = {post.content}
-            likes = {post.likes}
-            comments = {post.comments}
-            />
-        ))
+
+    render(){
+    
+       const postComponents =  this.state.posts.map(post => (
+             <Post
+              key = {'post-'+post.id}
+              name = {post.name}
+              date = {post.date}
+              content = {post.content}
+              likes = {post.likes}
+              comments = {post.comments}
+             />
+       ))
         return (
             <div>
-               {postComponents}
+            {postComponents}
             </div>
         );
     }
--- src/index.js
+++ src/index.js
@@ -23,7 +23,7 @@ ReactDOM.render(
         <Route path='/signup' component = {Signup}/>
         <Route component={NotFound} />
       </Switch>
-      {/* <PostList/> */}
+      {/* { <PostList/> } */}
     </Router>
     
   </React.StrictMode>,
